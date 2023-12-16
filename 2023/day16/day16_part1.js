78da74d614f102a7a025c7ad23039333840278e4
jorgelamb <jorgealvaro@gmail.com>
2023-12-16 11:00:00 +0100

    2023/16

---
+++ 2023/day16/day16_part1.js
@@ -0,0 +1,83 @@
+var map = input.split("\n").map(l => l.split("").map(e => { return { c: e, energized: false }; }));
+
+var pending = [[0, 0, "E"]];
+var processed = [];
+var result = 0;
+
+while(pending.length>0) {
+  var current = pending.pop();
+  if(processed.filter(e => e[0]==current[0] && e[1]==current[1] && e[2]==current[2]).length>0) {
+    continue;
+  }
+  
+  if(current[0]<0 || current[0]>=map.length || current[1]<0 || current[1]>=map[0].length) {
+    continue;
+  }
+
+  if(!map[current[0]][current[1]].energized) {
+  	map[current[0]][current[1]].energized = true;
+    result++;
+  }
+
+  switch(map[current[0]][current[1]].c) {
+    case ".":
+      switch(current[2]) {
+        case "E": pending.push([current[0], current[1]+1, current[2]]); break;
+        case "S": pending.push([current[0]+1, current[1], current[2]]); break;
+        case "W": pending.push([current[0], current[1]-1, current[2]]); break;
+        case "N": pending.push([current[0]-1, current[1], current[2]]); break;
+      }
+      break;
+
+    case "\\":
+      switch(current[2]) {
+        case "E": pending.push([current[0]+1, current[1], "S"]); break;
+        case "S": pending.push([current[0], current[1]+1, "E"]); break;
+        case "W": pending.push([current[0]-1, current[1], "N"]); break;
+        case "N": pending.push([current[0], current[1]-1, "W"]); break;
+      }
+      break;
+
+    case "/":
+      switch(current[2]) {
+        case "E": pending.push([current[0]-1, current[1], "N"]); break;
+        case "S": pending.push([current[0], current[1]-1, "W"]); break;
+        case "W": pending.push([current[0]+1, current[1], "S"]); break;
+        case "N": pending.push([current[0], current[1]+1, "E"]); break;
+      }
+      break;
+
+    case "-":
+      switch(current[2]) {
+        case "E": pending.push([current[0], current[1]+1, current[2]]); break;
+        case "S":
+          pending.push([current[0], current[1]+1, "E"]);
+          pending.push([current[0], current[1]-1, "W"]);
+          break;
+        case "W": pending.push([current[0], current[1]-1, current[2]]); break;
+        case "N":
+          pending.push([current[0], current[1]-1, "W"]);
+          pending.push([current[0], current[1]+1, "E"]);
+          break;
+      }
+      break;
+
+    case "|":
+      switch(current[2]) {
+        case "E":
+          pending.push([current[0]+1, current[1], "S"]);
+          pending.push([current[0]-1, current[1], "N"]);
+          break;
+        case "S": pending.push([current[0]+1, current[1], current[2]]); break;
+        case "W":
+          pending.push([current[0]-1, current[1], "N"]);
+          pending.push([current[0]+1, current[1], "S"]);
+          break;
+        case "N": pending.push([current[0]-1, current[1], current[2]]); break;
+      }
+      break;
+  }
+  processed.push(current);
+}
+
+log(`result: ${result}`);
